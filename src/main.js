df5328f9b4f08619c934a304368f4214da79b11e
Ethan <ethankeyser05@gmail.com>
2023-05-17 23:28:25 -0700

    menu skeleton added

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 let config = {
     type: Phaser.AUTO,
-    width: 700,
-    height: 700,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1920,
+        height: 1080
+    },
     physics: {
         default: "arcade",
         arcade: {
             debug: true
         }
     },
-    scene: [ Menu ],
+    scene: [ Menu, ],
 }
 
 let game = new Phaser.Game(config);
--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -1,12 +1,11 @@
-class Menu extends Phaser.scene {
+class Menu extends Phaser.Scene {
     constructor() {
         super('menu');
     }
-    preload() {
-
-    }
     create() {
-        let title = this.add.text(this.w, this.w, "testing");
-        title.setFontSize(this.s * 3);
+        this.cameras.main.setBackgroundColor("#ffffff");
+        this.add.text(700, 240, " Sketch \nand Roll", {color: "#000000"}).setFontSize(100);
+        this.add.text(800, 450, "Start Game", {color: "#000000"}).setFontSize(50);
+        this.add.text(775, 515, "Level Select", {color: "#000000"}).setFontSize(50);
     }
 }
