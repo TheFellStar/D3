6e760b7bbb51f4361b366a747e5a8e2faf5c697f
Ethan <ethankeyser05@gmail.com>
2023-05-17 22:19:27 -0700

    Added menu scene

---
+++ src/main.js
@@ -0,0 +1,20 @@
+let config = {
+    type: Phaser.AUTO,
+    width: 700,
+    height: 700,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
+    },
+    scene: [ Menu, BodyBumps, HoneySpider, Collider ],
+}
+
+let game = new Phaser.Game(config);
+
+let centerX = game.config.width/2;
+let widthSpacer = game.config.width/5;
+let halfHeight = game.config.height/2;
+let swap = null;
+let cursors = null;
